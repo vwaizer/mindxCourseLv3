c66aa8067c84335a6da54877c0e6755ea77ba34b
vwaizer <lightwing2208@gmail.com>
2023-11-28 10:05:28 +0700

    complete homework

--- mindxLV3/main.js
+++ mindxLV3/main.js
@@ -10,14 +10,22 @@ import process from "process"
 import { addStudent } from "./ultis/ultis.js"
 
 import cors from "cors"
+import multer from "multer"
+
 import { studentRoute } from "./routes/studentRoute.js"
+import { storage, uploadMiddle } from "./middleware/uploadMiddleware.js"
+
 const app = express()
 const port = 3000
 
 app.use(morgan('combined'))
 app.use(express.json())
 app.use(cors())
-
+app.post("/upload",uploadMiddle.single("avatar"),(req,res)=>{
+  let a =fs.readFileSync(`./upload/img/${req.file.filename}`,"base64");
+  fs.unlinkSync(`./upload/img/${req.file.filename}`)
+  return res.json(a);
+})
 app.use("/",studentRoute)
 
 
---
+++ mindxLV3/middleware/uploadMiddleware.js
@@ -0,0 +1,19 @@
+import multer from "multer";
+import path from "path";
+import fs from "fs";
+
+
+export const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, path.resolve("upload/img"));
+    },
+    filename: function (req, file, cb) {
+        console.log(file);
+        
+      const uniqueSuffix = Date.now() 
+      cb(null,  uniqueSuffix+'-' +file.originalname) 
+        
+      
+    }
+  })
+  export const uploadMiddle = multer({ storage: storage })
