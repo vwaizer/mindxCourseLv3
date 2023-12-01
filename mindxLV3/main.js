658cd58f827085e69918ddbf80d7ad4ad20e849e
vwaizer <lightwing2208@gmail.com>
2023-12-01 19:48:36 +0700

    upload

--- mindxLV3/main.js
+++ mindxLV3/main.js
@@ -8,13 +8,24 @@ import morgan from "morgan"
 import fs from "fs"
 import process from "process"
 import { addStudent } from "./ultis/ultis.js"
+import cookieParser from "cookie-parser";
+import session from "express-session";
 import cors from "cors"
 const app = express()
 const port = 3000
 const p={"id":5}
-app.use(morgan('combined'))
-app.use(express.json())
-app.use(cors())
+app.use(morgan('combined'));
+app.use(express.json());
+app.use(cors());
+app.use(cookieParser());
+require("dotenv").config(); 
+
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: true }
+}))
 app.post('/', (req, res) => {
   let a=fs.readFileSync("students.json");
   addStudent(...req.body,a);
